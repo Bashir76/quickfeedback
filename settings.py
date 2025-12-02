0f4d47b6de4bb375444fc06f11077a9c199ee3d1
Bashir, Amir Kano <99528741+Bashir76@users.noreply.github.com>
2025-12-02 08:23:52 +0100

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,1 +1,22 @@
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'rest_framework',
+    'core',
+]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'quickfeedback',
+        'USER': 'postgres',
+        'PASSWORD': 'yourpassword',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
 
