252e9d5aa105b9aae59a1d86e95688118facea66
Bashir, Amir Kano <99528741+Bashir76@users.noreply.github.com>
2025-12-02 08:44:29 +0100

    Add admin-only POST permissions for views
    Restrict POST permissions for Category and Item views to admin users only.

--- core/views.py
+++ core/views.py
@@ -2,16 +2,26 @@ from rest_framework import generics
 from .models import Category, Item, Feedback
 from .serializers import CategorySerializer, ItemSerializer, FeedbackSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
-
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 class CategoryListCreateView(generics.ListCreateAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
+    def get_permissions(self):
+        if self.request.method == "POST":
+            return [IsAdminUser()]
+        return []
+        
 
 class ItemListCreateView(generics.ListCreateAPIView):
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
+
+    def get_permissions(self):
+        if self.request.method == "POST":
+            return [IsAdminUser()]
+        return []
 
 
 class ItemDetailView(generics.RetrieveAPIView):
